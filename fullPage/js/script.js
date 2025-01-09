b9d5937445233d89f4e2933e50a2308aba8f3f1f
miyuu6123 <157795023+miyuu6123@users.noreply.github.com>
2025-01-09 15:20:56 +0900

    Add files via upload

---
+++ fullPage/js/script.js
@@ -0,0 +1,17 @@
+$("#fullpage").fullpage( {
+    autoScrolling: true,
+    navigation: true,
+    anchors: [
+        "firstPage",
+        "secondPage",
+        "thirdPage",
+        "fourthPage",
+    ],
+    sectionsColor: [
+        "#ff5f44",
+        "#0798ec",
+        "#fc6c7c",
+        "#005FFF",
+    ],
+    responsiveWidth: 900,
+});
